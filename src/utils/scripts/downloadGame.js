dd403aae63e3a6ddc26811385c1f05aeafe6832f
loockeeer <lulu.r1313@gmail.com>
2020-12-05 20:36:09 +0100

    Start implement download-server client

--- src/utils/scripts/downloadGame.js
+++ src/utils/scripts/downloadGame.js
@@ -1,5 +1,30 @@
-export default function downloadGame({url, path}) {
-    // Clear folder where game sits with fs
-    // Download game in temp folder
-    // Unzip game in game folder
+import Axios from 'axios'
+import walk from './walk'
+
+export default async function downloadGame({url, path, fb}) {
+    const files = []
+    for await(const file of walk(path)) {
+        const hash = await hasha.fromFile(fil, { algorithm: 'sha1' })
+        files.push({
+            relativePath: file.replace(folder.endsWith('/') ? folder : `${folder}/`, ''),
+            hash
+        })
+    }
+
+    const toDownloadFiles = await axios({
+        url: `${url}/compare`,
+        method: 'POST',
+        data: { files }
+    }).then(res => res.data).catch(res => { throw res?.body?.message })
+
+
+    for(const toDownload of toDownloadFiles) {
+        await fs.promises.mkdir(path.dirname(path.join(folder, toDownload.relativePath)), { recursive: true })
+        const file = fs.createWriteStream(path.join(folder, toDownload.relativePath))
+
+        http.get(`${url}/download/${toDownload.relativePath}`, res => {
+            res.pipe(file)
+            res.on('end', () => fb(toDownload.relativePath, toDownloadFiles.length))
+        })
+    }
 }
---
+++ src/utils/scripts/walk.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const fs = require('fs/promises');
+
+async function* walk(dir) {
+  const dirents = await fs.readdir(dir, { withFileTypes: true });
+  for (const dirent of dirents) {
+    const res = path.resolve(dir, dirent.name);
+    if (dirent.isDirectory()) {
+      yield* walk(res);
+    } else {
+      yield res;
+    }
+  }
+}
+
+export default walk;
