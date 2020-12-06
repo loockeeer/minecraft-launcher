b5f34ba1df1417cdfbfa92e9cea7bdf96b6c0d10
loockeeer <lulu.r1313@gmail.com>
2020-12-06 11:22:50 +0100

    Remove minecraftmanager because it was useless

--- src/utils/MinecraftManager.js
+++
@@ -1,6 +0,0 @@
-import Store from './StoreManager';
-import config from '../config';
-import launchMinecraft from './scripts/launchMinecraft';
-import downloadGame from './scripts/downloadGame';
-
-const store = new Store()
