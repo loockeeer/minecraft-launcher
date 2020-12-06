9da096c47a559211dbef1101cea057d70cd7bc4b
loockeeer <lulu.r1313@gmail.com>
2020-12-06 17:19:40 +0100

    Add strings for load game

---
+++ src/strings/LoadGame.js
@@ -0,0 +1,9 @@
+export default {
+  // texts
+  load__startDownload: 'Initialisation du téléchargement des fichiers',
+  load__startCheckHash: 'Vérification de l\'intégrité des fichiers',
+  load__startGame: 'Lancement du jeu',
+
+  // errors
+  load__error__download__server__500: 'Le serveur de téléchargement ne fonctionne pas. Veuillez ré-essayer ultérieurement',
+};
