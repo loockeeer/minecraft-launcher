9d0e385edc67b08144d6635461f1dd6ac9549511
loockeeer <lulu.r1313@gmail.com>
2020-12-06 19:19:42 +0100

    Add a dynamic string for play button

--- src/strings/Home.js
+++ src/strings/Home.js
@@ -3,6 +3,7 @@ export default {
   home__news__loading: 'Please wait...',
   home__load__startDownload: 'Initialisation du téléchargement des fichiers',
   home__load__startGame: 'Lancement du jeu',
+  home__play__button: 'play',
 
   // errors
   home__load__error__download__server__500: 'Le serveur de téléchargement ne fonctionne pas. Veuillez ré-essayer ultérieurement',
