0388e7c71a9650be5c6f1e45147de2130b61ab70
loockeeer <lulu.r1313@gmail.com>
2020-12-06 11:20:38 +0100

    Add redirect to PlayPage

--- src/components/PlayButton.jsx
+++ src/components/PlayButton.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import { Redirect } from 'react-router';
 import '../css/PlayButton.css';
 
 class PlayButton extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { play: false };
   }
 
   render() {
+    const { play } = this.state;
     return (
-      <a href="#" className="PlayButton">JOUER</a>
+      <div>
+        <a href="#" className="PlayButton" onClick={()=>this.setState({ play: true})}>JOUER</a>
+        { play && (<Redirect to="/play"></Redirect>)}
+      </div>
     );
   }
 }
