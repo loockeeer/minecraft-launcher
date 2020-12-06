abe97f6a17a46f4b42c33c1d8c567d0c23c53537
loockeeer <lulu.r1313@gmail.com>
2020-12-06 15:52:04 +0100

    Add downloadGame

--- src/components/LoadGame.jsx
+++ src/components/LoadGame.jsx
@@ -1,23 +1,36 @@
 import React from 'react';
+import config from '../config.js';
 import downloadGame from '../utils/scripts/downloadGame';
 import '../css/LoadGame.css';
 
 class PlayButton extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { progressValue: 50, label: 'salut' };
+    this.state = { progressValue: 50, label: 'salut', progressMax: 10 };
   }
 
   componentDidMount() {
     // Start downloading the game
+    downloadGame({
+      url: config.downladServerURL,
+      path: config.gamePath,
+      fb: (file, total) => {
+        const { progressValue } = this.state;
+        this.setState({
+          progressMax: total,
+          progressValue: progressValue + 1,
+          label: file,
+        });
+      },
+    });
   }
 
   render() {
-    const { label, progressValue } = this.state;
+    const { label, progressValue, progressMax } = this.state;
     return (
       <div>
         <p>{label}</p>
-        <progress id="progress" max="500" value={progressValue} />
+        <progress id="progress" max={progressMax} value={progressValue} />
       </div>
     );
   }
