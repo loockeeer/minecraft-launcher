62a978cc6e2134ae0d3a3dfb014d3da961fe94ca
loockeeer <lulu.r1313@gmail.com>
2020-12-06 15:06:19 +0100

    Add progress bar to playbutton component

--- src/components/PlayButton.jsx
+++ src/components/PlayButton.jsx
@@ -5,15 +5,22 @@ import '../css/PlayButton.css';
 class PlayButton extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { play: false };
+    this.state = { play: false, progressValue: 50, label: 'salut'};
+  }
+  componentDidMount() {
   }
 
   render() {
-    const { play } = this.state;
+    const { play, progressValue, label } = this.state;
     return (
       <div>
-        <a href="#" className="PlayButton" onClick={()=>this.setState({ play: true})}>JOUER</a>
-        { play && (<Redirect to="/play"></Redirect>)}
+        { !play && <a href="#" className="PlayButton" onClick={()=>this.setState({ play: true})}>JOUER</a>}
+        { play && (
+        <div>
+          <p>{label}</p>
+          <progress id="progress" max="500" value={progressValue} />
+        </div>
+        )}
       </div>
     );
   }
