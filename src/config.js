ea0182914214d62f46493dbf809a94bbfaf3e38b
loockeeer <lulu.r1313@gmail.com>
2020-12-06 15:25:42 +0100

    Add gamePath in config.Js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,7 @@
+const app = window.require('electron').remote;
+const electron = app.require('electron');
+const path = app.require('path');
+
 export default {
   name: 'OkinaGame',
   defaults: {
@@ -9,4 +13,5 @@ export default {
   },
   yggdrasilServerURL: 'https://authserver.mojang.com/',
   newsURL: 'https://hastebin.com/raw/cigibewopo',
+  gamePath: path.join((electron.app || electron.remote.app).getPath('userData'), 'game'),
 };
