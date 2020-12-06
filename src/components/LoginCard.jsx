55518423bc55e26fe9c8619943e774493ac0e3cd
loockeeer <lulu.r1313@gmail.com>
2020-12-06 15:26:14 +0100

    Lint

--- src/components/LoginCard.jsx
+++ src/components/LoginCard.jsx
@@ -6,7 +6,7 @@ import LoginStrings from '../strings/Login';
 class LoginCard extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { username: '', password: '', rememberme: false, username_error: false, password_error: false, submit_button_disabled: false };
+    this.state = { username: '', password: '', rememberme: false, usernameError: false, passwordError: false, submitButtonDisabled: false };
   }
 
   componentDidMount() {
@@ -25,9 +25,8 @@ class LoginCard extends React.Component {
   }
 
   render() {
-    // eslint-disable-next-line react/prop-types
     const { submitCredentials, submitButtonDisabled } = this.props;
-    const { username_error, password_error } = this.state;
+    const { usernameError, passwordError } = this.state;
     return (
       <div className="LoginCardContainer">
         <div className="LoginCard">
@@ -36,33 +35,32 @@ class LoginCard extends React.Component {
             <div className="LoginCard__head__title">{LoginStrings.login__bar__title}</div>
           </div>
           <form className="LoginCard__form">
-            <input onChange={(e) => this.handleChangeUsername(e)} type="text" className={`LoginCard__form__username ${username_error && "input-error"} noBorderInput`} id="LoginCard__form__username" placeholder={LoginStrings.login__bar__username} />
-            <input onChange={(e) => this.handleChangePassword(e)} type="password" className={`LoginCard__form__password ${password_error && "input-error"} noBorderInput`} id="LoginCard__form__password" placeholder={LoginStrings.login__bar__password} />
+            <input onChange={(e) => this.handleChangeUsername(e)} type="text" className={`LoginCard__form__username ${usernameError && 'input-error'} noBorderInput`} id="LoginCard__form__username" placeholder={LoginStrings.login__bar__username} />
+            <input onChange={(e) => this.handleChangePassword(e)} type="password" className={`LoginCard__form__password ${passwordError && 'input-error'} noBorderInput`} id="LoginCard__form__password" placeholder={LoginStrings.login__bar__password} />
             <div className="LoginCard__form__rememberme">
               <input onChange={(e) => this.handleChangeCheckbox(e)} type="checkbox" />
               {LoginStrings.login__bar__remember_me}
             </div>
-            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-            <button 
+            <button
               onClick={(e) => {
                 e.preventDefault();
                 const { username, password, rememberme } = this.state;
-                if (!username) this.setState({ username_error: true })
-                else this.setState({ username_error: false })
+                if (!username) this.setState({ usernameError: true });
+                else this.setState({ usernameError: false });
 
-                if (!password) this.setState({ password_error: true })
-                else this.setState({ password_error: false })
+                if (!password) this.setState({ passwordError: true });
+                else this.setState({ passwordError: false });
 
                 if (username && password) submitCredentials({ username, password, rememberme });
               }}
               href="#"
               className="LoginCard__submit"
               disabled={submitButtonDisabled}
+              type="submit"
             >
               {LoginStrings.login__bar__login}
             </button>
           </form>
-          {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
           <a
             href="#"
             onClick={async (e) => {
