39a294b93c4407c5a7a158df3858329a17d86ce0
loockeeer <lulu.r1313@gmail.com>
2020-12-17 21:37:43 +0100

    Change news loading text

--- src/strings/Home.js
+++ src/strings/Home.js
@@ -1,6 +1,6 @@
 export default {
   // texts
-  home__news__loading: 'Please wait...',
+  home__news__loading: '...',
   home__load__startDownload: 'Initialisation du téléchargement des fichiers',
   home__load__startGame: 'Lancement du jeu',
   home__play__button: 'play',
