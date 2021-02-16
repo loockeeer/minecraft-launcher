c173301151136cf46050bcb3b8de4360cdce756e
loockeeer <lulu.r1313@gmail.com>
2021-02-16 17:24:34 +0100

    feat(toolbox): add disconnect and settings buttons

--- src/components/ToolBox.jsx
+++ src/components/ToolBox.jsx
@@ -1,19 +1,49 @@
 import React from 'react';
+import { Redirect } from 'react-router';
 import '../css/ToolBox.css';
 import UserInfo from './UserInfo';
+import Store from '../utils/StoreManager';
 
 class ToolBox extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.store = new Store();
+    // redirect = 1 => /login
+    // redirect = 2 => /settings/java
+    this.state = {
+      redirect: 0,
+    };
   }
 
   render() {
+    const { redirect } = this.state;
     return (
       <div className="ToolBox">
         <UserInfo />
-        <p className="ToolBox__button">DECONNECTION</p>
-        <p className="ToolBox__button">PARAMETRES</p>
+        <a
+          href="#"
+          onClick={(e) => {
+            e.preventDefault();
+            this.store.setAccessToken(undefined);
+            this.store.setClientToken(undefined);
+            this.store.setUserInfo({ name: undefined, id: undefined });
+            this.setState({ redirect: 1 });
+          }}
+          className="ToolBox__button"
+        >
+          DECONNECTION
+        </a>
+        <a
+          href="#"
+          onClick={(e) => {
+            e.preventDefault();
+            this.setState({ redirect: 2 });
+          }}
+          className="ToolBox__button"
+        >
+          PARAMETRES
+        </a>
+        { redirect === 1 ? <Redirect to="/login" /> : redirect === 2 && <Redirect to="/settings/java" /> }
       </div>
     );
   }
