79b7cc7729314b1e3472d0327907002b423fbc37
loockeeer <lulu.r1313@gmail.com>
2021-02-16 17:03:09 +0100

    feat(homePage): add userinfo

---
+++ src/components/UserInfo.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../css/UserInfo.css';
+import Store from '../utils/StoreManager';
+
+class UserInfo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.store = new Store();
+  }
+
+  render() {
+    const { name, id } = this.store.getUserInfo();
+    const avatarURL = `https://crafatar.com/avatars/${id}`;
+    return (
+      <div className="UserInfo">
+        <p className="UserInfo__username">{name}</p>
+        <img alt="avatar" className="UserInfo__avatarURL" src={avatarURL} />
+      </div>
+    );
+  }
+}
+export default UserInfo;
