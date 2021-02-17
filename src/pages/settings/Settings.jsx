f9a8f448b65884b5b022bcb3d800fdd98c630900
loockeeer <lulu.r1313@gmail.com>
2021-02-17 19:21:20 +0100

    feat(settings): add save and close buttons

--- src/pages/settings/Settings.jsx
+++ src/pages/settings/Settings.jsx
@@ -6,24 +6,30 @@ import {
   Link,
 } from 'react-router-dom';
 import JavaSettings from './JavaSettings';
-import Update from './Update';
 import About from './About';
+import Store from '../../utils/StoreManager';
 import '../../css/Settings.css';
 
 class Settings extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.store = new Store();
+    this.state = {
+      // eslint-disable-next-line react/no-unused-state
+      maxRam: undefined,
+      // eslint-disable-next-line react/no-unused-state
+      javaPath: undefined,
+    };
   }
 
   render() {
+    const { javaPath, maxRam } = this.state;
     return (
       <div className="Settings">
         <div className="Settings__menu">
           <div className="Settings__menu__sub">
             <h1 className="Settings__title Settings__menu__sub__link">Paramètres</h1>
             <Link to="/settings/java" className="Settings__menu__sub__link">Java</Link>
-            <Link to="/settings/update" className="Settings__menu__sub__link">Mise à jour</Link>
           </div>
           <div className="Settings__menu__sub">
             <Link to="/settings/about" className="Settings__menu__sub__link">A propos</Link>
@@ -34,17 +40,41 @@ class Settings extends React.Component {
           <Switch>
             <Route path="/settings/java" exact>
               <h1 className="Settings__switch__title">Paramètres Java</h1>
-              <JavaSettings />
-            </Route>
-            <Route path="/settings/update" exact>
-              <h1 className="Settings__switch__title">Mise à jour</h1>
-              <Update />
+              <JavaSettings
+                className="Settings__page"
+                onRAMChange={(value) => {
+                  /* eslint-disable-next-line react/no-unused-state */
+                  this.setState({ maxRam: value });
+                }}
+                onJavaPathChange={(value) => {
+                  /* eslint-disable-next-line react/no-unused-state */
+                  this.setState({ javaPath: value });
+                }}
+              />
             </Route>
             <Route path="/settings/about" exact>
               <h1 className="Settings__switch__title">A propos</h1>
               <About />
             </Route>
           </Switch>
+          <div className="Settings__switch__buttons">
+            <a
+              href="#"
+              onClick={(e) => {
+                e.preventDefault();
+                if (javaPath !== undefined) {
+                  this.store.setJavaPath(javaPath);
+                }
+                if (maxRam !== undefined) {
+                  this.store.setMaxRam(maxRam);
+                }
+              }}
+              className="Settings__switch__buttons__button"
+            >
+              APPLIQUER
+            </a>
+            <Link to="/" className="Settings__switch__buttons__button">QUITTER</Link>
+          </div>
         </div>
       </div>
     );
