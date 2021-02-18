c2a97337910f27de42a5668c00281924ad0683ea
loockeeer <lulu.r1313@gmail.com>
2021-02-18 20:14:08 +0100

    fix(getHostRAM): add Math.floor

--- src/utils/scripts/getHostRAM.js
+++ src/utils/scripts/getHostRAM.js
@@ -5,12 +5,12 @@ export default function getHostRAM(unit) {
   const mem = os.totalmem();
   switch (unit) {
     case 'kb':
-      return mem / 1024;
+      return Math.floor(mem / 1024);
     case 'mb':
-      return mem / 1024 / 1024;
+      return Math.floor(mem / 1024 / 1024);
     case 'gb':
-      return mem * (1e-9);
+      return Math.floor(mem * (1e-9));
     default:
-      return mem;
+      return Math.floor(mem);
   }
 }
